988c0679fc4e1f63e97a5cee7ef8be1395192846
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2016-03-05 15:44:10 +0200

    answer controller

---
+++ controllers/answer.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Boom = require('boom');
+var AnswerModel = require('../models/answer');
+
+
+function AnswerController(db) {
+    this.answerModel = new AnswerModel(db);
+};
+
+AnswerController.prototype.answer = function(request, reply) {
+    try {
+     
+    
+
+    } catch (e) {
+        reply(Boom.badRequest(e.message));
+    }
+
+};
+
+
+
+module.exports = AnswerController;
